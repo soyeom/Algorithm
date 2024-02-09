8299904eafcce574a87fc1633b94832a7bf64834
박소연 <psoyeon0127@naver.com>
2024-02-09 15:15:35 +0900

    반복, 조건문

---
+++ src/Statement.java
@@ -0,0 +1,16 @@
+public class Statement {
+    public static void main(String[] args) {
+        int a=5;
+        int b=2;
+        int c;
+        int d;
+
+        if(a>b) {
+            System.out.println("Hello");
+        }
+
+        for(int i =1; i<=10; i++) {
+            System.out.printf("%d * %d = %d", a, i, a*i).println();
+        }
+    }
+}
