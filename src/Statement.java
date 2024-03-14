0bd70822a073672394896e5bde96edb417d5b1ae
박소연 <psoyeon0127@naver.com>
2024-03-14 14:49:19 +0900

    if-else

--- src/Statement.java
+++ src/Statement.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Statement {
     public static void main(String[] args) {
-        int a=5;
-        int b=2;
         int c;
         int d;
 
-        if(a>b) {
-            System.out.println("Hello");
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        NestedIfElse(a, b);
 
         for(int i =1; i<=10; i++) {
             System.out.printf("%d * %d = %d", a, i, a*i).println();
+        }
+    }
+
+    private static void NestedIfElse(int a, int b) {
+        if(a > b) {
+            System.out.println("Hello");
+        }
+        else if(a == b) {
+            System.out.println("Hi");
+        }
+        else {
+            System.out.println("Hey");
         }
     }
 }
